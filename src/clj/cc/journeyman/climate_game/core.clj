6f4a23423dc759a7979d3ec6de3b61ee5464103b
Simon Brooke <simon@journeyman.cc>
2021-11-18 14:22:46 +0000

    Game launches and exits cleanly, nothing actually works.

--- src/clj/cc/journeyman/climate_game/core.clj
+++ src/clj/cc/journeyman/climate_game/core.clj
@@ -3,8 +3,16 @@
   (:require [cc.journeyman.climate-game.planet.geometry :refer [init-planet]]
             [jme-clj.core :refer [add-light-to-root add-to-root box defsimpleapp generate geo get*
                                   light load-texture material rotate set* sphere start vec3]])
-  (:import [cc.journeyman.climate_game ClimateGameApp]))
+  (:import [cc.journeyman.climate_game ClimateGameApp])
+  (:gen-class))
 
-(def app (ClimateGameApp.))
 
-(start app)
+(defn -main
+  "This should be pretty simple."
+  [& args]
+  (let [app (ClimateGameApp.)]
+    (try
+      (start app)
+      (catch Exception any
+        (println (.getMessage any))))))
+
